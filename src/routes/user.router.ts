997ddc952ad730a1db0b0505a3d06f29c5f1faea
Kostya <k.gorodnichuk@gmail.com>
2022-11-19 08:50:10 +0300

    add: route /

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -4,6 +4,9 @@ import { Router } from 'express'
 import { credentialVerification } from '../middleware/credentialVerification'
 
 export const createUserRouter = (router: Router, userController: IUserController): Router => {
+  router.get('/', (req, res) => {
+    res.status(200).send('Ok')
+  })
   router.get('/validate', credentialVerification, userController.validateToken)
   router.post('/register', userController.register)
   router.post('/login', userController.login)
